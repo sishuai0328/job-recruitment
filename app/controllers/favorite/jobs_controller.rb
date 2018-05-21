857ce3a569c74fd209460d868a6a30cd84cb7436
sish <sishuai0328@gmail.com>
2018-05-21 21:54:29 +0800

    update my favorite job

--- app/controllers/favorite/jobs_controller.rb
+++ app/controllers/favorite/jobs_controller.rb
@@ -1,9 +1,14 @@
 class Favorite::JobsController < ApplicationController
-  
-  before_action :authenticate_user!
-  def index
-    @jobs = current_user.favorite_jobs #列出当前用户收藏的所有工作
 
+  before_action :authenticate_user!
+  # def index
+  #   @jobs = current_user.favorite_jobs #列出当前用户收藏的所有工作
+  #
+  # end
+
+  def index
+    @user = current_user
+    @job_favorites = JobFavorite.where(:user => @user).order("created_at DESC").paginate(:page => params[:page], :per_page => 7)
   end
 
 end
